25be8a720d0d3c2eb0e7392eaf00c218514b6e7a
Igor Vaiman <igor.vaiman@inten.to>
2024-07-15 14:23:38 +0200

    minor

--- api/types/currency.py
+++ api/types/currency.py
@@ -9,12 +9,13 @@ from api.types.currency_iso4217 import CurrencyISO4217
 def parse_currency(v: Any) -> CurrencyISO4217:
     if isinstance(v, CurrencyISO4217):
         return v
-    if not isinstance(v, str):
+    if isinstance(v, str):
+        v = v.upper()
+        if v not in CURRENCIES:
+            raise ValueError(f"not a valid ISO 4217 code: {v}")
+        return CURRENCIES[v]
+    else:
         raise TypeError(f"currency value must be a string containing three-letter ISO2417 code")
-    v = v.upper()
-    if v not in CURRENCIES:
-        raise ValueError(f"not a valid ISO 4217 code: {v}")
-    return CURRENCIES[v]
 
 
 def dump_currency(c: CurrencyISO4217):
